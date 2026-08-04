18bd99ee2a1f972d2000a7d17b91e0eae9ba19c9
Tangxinyan0904 <378186933@qq.com>
2026-08-05 04:46:03 +0800

    chore(web): add required copyright headers

--- web/src/features/violation-records/components/__tests__/violation-records-table.test.tsx
+++ web/src/features/violation-records/components/__tests__/violation-records-table.test.tsx
@@ -1,4 +1,3 @@
-import { afterAll, afterEach, beforeEach, mock, test } from 'bun:test'
 /*
 Copyright (C) 2023-2026 QuantumNous
 
@@ -17,6 +16,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 For commercial licensing, please contact support@quantumnous.com
 */
+import { afterAll, afterEach, beforeEach, mock, test } from 'bun:test'
 import assert from 'node:assert/strict'
 
 import { Window } from 'happy-dom'
